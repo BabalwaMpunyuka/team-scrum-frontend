4da26a2ae4186e78fa069b226771f1484a1f670f
Evelyn Anyebe <anyebeevelyn@gmail.com>
2021-09-29 11:33:07 +0100

    Add portfolio images

--- src/pages/portfolio/serviceItems.js
+++ src/pages/portfolio/serviceItems.js
@@ -8,11 +8,66 @@ export const serviceItems = [
     items: [
       {
         title: "",
-        src: "/images/portfolio/financial-appraisals.png",
+        src: "/images/portfolio/financial_appraisal_1.png",
         description: ``,
         id: "1",
-        imgClass: "col-sm-12",
-        descClass: "col-sm-12",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_appraisal_2.png",
+        description: ``,
+        id: "2",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_appraisal_3.png",
+        description: ``,
+        id: "3",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_appraisal_4.png",
+        description: ``,
+        id: "4",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_appraisal_5.png",
+        description: ``,
+        id: "5",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_appraisal_6.png",
+        description: ``,
+        id: "6",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_appraisal_7.png",
+        description: ``,
+        id: "7",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_appraisal_9.png",
+        description: ``,
+        id: "p",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_appraisal_10.png",
+        description: ``,
+        id: "10",
+        
       },
     ],
   },
@@ -28,11 +83,17 @@ export const serviceItems = [
     items: [
       {
         title: "",
-        src: "/images/portfolio/financial-diagnostics.png",
+        src: "/images/portfolio/financial_diagnostics_apple.PNG",
         description: ``,
         id: "1",
-        imgClass: "col-sm-12",
-        descClass: "col-sm-12",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_diagnostics_microsoft.PNG",
+        description: ``,
+        id: "2",
+        
       },
     ],
   },
@@ -41,29 +102,55 @@ export const serviceItems = [
     serviceTitle: "",
     serviceDesc: `Financial modeling is a representation in numbers of a company's operations in the past, present, and the forecasted future. Such models are intended to be used as decision-making tools. In Financial analysis they are used to explain or anticipate the impact of events on a company's stock, from internal factors, such as a change of strategy or business model to external factors such as a change in economic policy or regulation.`,
     embedItems: [
-      {
-        id: "1",
-        title: "Microsoft Financial modelling",
-        embedTag: (
-          <embed src="https://app.powerbi.com/reportEmbed?reportId=27f3c19d-a78b-44c4-9fc4-b3f59771cd9f&autoAuth=true&ctid=682106db-9a7b-4b35-b9a0-88473385d6cf&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" />
-        ),
-      },
-      {
-        id: "2",
-        title: "Apple Financial modelling",
-        embedTag: (
-          <embed src="https://app.powerbi.com/reportEmbed?reportId=ba7730ce-a781-44c5-94c4-40c26c6cc419&autoAuth=true&ctid=682106db-9a7b-4b35-b9a0-88473385d6cf&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" />
-        ),
-      },
+      // {
+      //   id: "1",
+      //   title: "",
+      //   embedTag: (
+      //     <embed src="https://app.powerbi.com/reportEmbed?reportId=27f3c19d-a78b-44c4-9fc4-b3f59771cd9f&autoAuth=true&ctid=682106db-9a7b-4b35-b9a0-88473385d6cf&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" />
+      //   ),
+      // },
     ],
     items: [
       {
-        title: "Microsoft Financial modelling",
-        src: "/images/portfolio/microsoft_financial_modelling.png",
+        title: "",
+        src: "/images/portfolio/financial_modelling_1.PNG",
         description: ``,
         id: "1",
-        imgClass: "col-sm-12",
-        descClass: "col-sm-12",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_modelling_2.PNG",
+        description: ``,
+        id: "2",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_modelling_3.PNG",
+        description: ``,
+        id: "3",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_modelling_4.PNG",
+        description: ``,
+        id: "4",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_modelling_5.PNG",
+        description: ``,
+        id: "5",
+        
+      },
+      {
+        title: "",
+        src: "/images/portfolio/financial_modelling_6.PNG",
+        description: ``,
+        id: "6"
       },
     ],
   },
